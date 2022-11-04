b6a75256da1c0a13efb0f1a9146431cf685a5ff1
baimamboukar <baimamboukar@gmail.com>
2022-11-04 11:31:39 +0100

    Mboathoscope v1.0.0 🚀

--- commit.sh
+++ commit.sh
@@ -1,5 +1,5 @@
- for file in $(git ls-files --exclude-standard); do
-   git add $file
-   git commit -m "$1"
- done
- git push
+for file in $(git ls-files --modified --others --exclude-standard); do
+    git add $file
+    git commit -m "Mboathoscope v1.0.0 🚀"
+done
+git push
